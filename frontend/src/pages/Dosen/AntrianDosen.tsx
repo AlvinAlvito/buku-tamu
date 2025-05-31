346966da880a86bc111da83d7e2e45117bf98719
AlvinAlvito <alvinparis65@gmail.com>
2025-05-31 17:06:59 +0700

    add

--- frontend/src/pages/Dosen/AntrianDosen.tsx
+++ frontend/src/pages/Dosen/AntrianDosen.tsx
@@ -1,6 +1,5 @@
 import PageBreadcrumb from "../../components/common/PageBreadCrumb";
 import AntrianDosen from "../../components/dosen/AntrianDosen";
-import BuatJanji from "../../components/dosen/BuatJanji";
 import UserAddressCard from "../../components/UserProfile/UserAddressCard";
 export default function BasicTables() {
   return (
@@ -8,7 +7,6 @@ export default function BasicTables() {
 
       <PageBreadcrumb pageTitle="Dosen" />
       <div className="space-y-6">
-          <BuatJanji />
           <UserAddressCard/>
           <AntrianDosen />
       </div>
